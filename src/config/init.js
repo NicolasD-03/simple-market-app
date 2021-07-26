5d3f549d71dcce335cc590172653288462cfa167
Dick Nicolas <dick.nicolas57@gmail.com>
2021-07-26 21:42:17 +0200

    Remove index for name and add useFindAndModify: false

--- src/config/init.js
+++ src/config/init.js
@@ -4,6 +4,6 @@ const { dbHost, dbPort, dbName } = require('./index.js');
 mongoose.connect(`${ dbHost }:${ dbPort }/${ dbName }`, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useFindAndModify: false
 });
 
