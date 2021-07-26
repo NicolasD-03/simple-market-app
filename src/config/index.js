69f5b5a293a6956eb5b5d2f16f035ad280d5a755
Dick Nicolas <dick.nicolas57@gmail.com>
2021-07-26 03:10:40 +0200

    Retrive env info + set-up database connection

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const hostname = process.env.HOST;
+const port = process.env.SERVER_PORT;
+const apiAdress = process.env.API_ADRESS;
+const apiKey = process.env.API_KEY;
+
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+module.exports = {hostname, port, apiAdress, apiKey, dbHost, dbPort, dbName}
---
+++ src/config/init.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { dbHost, dbPort, dbName } = require('./index.js');
+
+mongoose.connect(`${ dbHost }:${ dbPort }/${ dbName }`, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
