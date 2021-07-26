e2e2000e98201d0ea8198e08526015965c2b4ed7
Dick Nicolas <dick.nicolas57@gmail.com>
2021-07-26 03:11:13 +0200

    Set-up main server and custom middleware for the API

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const cart = require('./routes/cart.js');
-const market = require('./routes/market.js');
-
+const cart  = require('./api/routes/cart.js');
+const market = require('./api/routes/market.js');
+const config = require('./config/index.js');
+const db = require('./config/init.js');
 const app = express();
 
-const port = 8080;
+const port = config.port || 8080;
+const host = config.hostname || "localhost";
+const apiAdress =  config.apiAdress;
 
 app.use(morgan('tiny'));
 app.use(express.static('public'));
-app.use(cart);
-app.use(market);
+app.use(express.json());
+
+app.use((`${ apiAdress }cart`), cart);
+app.use((`${ apiAdress }market`), market);
 
 
-app.listen(port, () => {
-    console.log(`Server start at http://localhost:${port}`);
+app.listen(port, host, () => {
+    console.log(`Server start at http://${ host }:${ port }`);
 });
