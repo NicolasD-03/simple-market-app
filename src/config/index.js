a6d2206f2c7fbd8f9890129509c1e09bbf9e9cf6
Dick Nicolas <dick.nicolas57@gmail.com>
2021-07-28 03:31:59 +0200

    Add jwt key

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,7 @@ const hostname = process.env.HOST;
 const port = process.env.SERVER_PORT;
 const apiAdress = process.env.API_ADRESS;
 const apiKey = process.env.API_KEY;
+const jwtKey = process.env.JWT_KEY;
 
 const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
@@ -11,4 +12,4 @@ const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
-module.exports = {hostname, port, apiAdress, apiKey, dbHost, dbPort, dbName}
+module.exports = {hostname, port, apiAdress, apiKey, jwtKey, dbHost, dbPort, dbName}
