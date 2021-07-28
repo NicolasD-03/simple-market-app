c83f2871924ae86f904b14a08ec8d7cb68599adf
Dick Nicolas <dick.nicolas57@gmail.com>
2021-07-28 03:31:34 +0200

    Add one options to database connection

--- src/config/init.js
+++ src/config/init.js
@@ -4,6 +4,7 @@ const { dbHost, dbPort, dbName } = require('./index.js');
 mongoose.connect(`${ dbHost }:${ dbPort }/${ dbName }`, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
