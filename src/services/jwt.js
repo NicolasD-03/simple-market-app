518207b513bd64adfcc2173fc7c2183c57ccac5d
Dick Nicolas <dick.nicolas57@gmail.com>
2021-07-30 00:04:36 +0200

    Add jwt creation and verification functions

---
+++ src/services/jwt.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const { jwtKey } = require('../config/index.js')
+
+const createToken = (userId, userUsername, userEmail, userAdmin, userApiKey, userEmailVerify) => {
+    const token = jwt.sign(
+         {
+             user_id: userId,
+             user_username: userUsername,
+             user_email: userEmail,
+             user_admin: userAdmin,
+             user_APIKey: userApiKey,
+             user_emailVerify: userEmailVerify
+         },
+         jwtKey,
+         {
+             expiresIn: "30m"
+         }
+    );
+    return token;
+};
+
+const verifyToken = (token) => {
+    const test = jwt.verify(token, jwtKey, (err, decoded) => {
+        if(err){
+            return {status: 400, message: "Token error"};
+        };
+
+        return {status: 200, data: decoded};
+    });
+    return test;
+        
+};
+
+module.exports = { createToken, verifyToken };
