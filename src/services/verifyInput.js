2c01beeab3c9ec4c57e1420f61eb4eaa186e332e
Dick Nicolas <dick.nicolas57@gmail.com>
2021-08-04 23:30:01 +0200

    refactor: Adding parameters
    Adding password confirm parameter in the input verification

--- src/services/verifyInput.js
+++ src/services/verifyInput.js
@@ -1,23 +1,26 @@
 const emailValidator = require('email-validator');
 
-const verifyRegisterInput = (username, password, email) => {
-    if(!(username && password && email)) {
-        return {status: 400, message: "All input is required"};
+const verifyRegisterInput = (username, password, passwordConfirm, email) => {
+    if(!(username && password && passwordConfirm && email)) {
+        return {status: 400, message: "All inputs are required"};
     }
     if(username.length > 20){
         return {status: 400, message: "Username is too long"};
     }
-    if(password.length < 8){
+    if(password.length < 8 || passwordConfirm.length < 8){
         return {status: 400, message: "Password is  too short"};
     }
     if(!emailValidator.validate(email)){
         return {status: 400, message: "Email is not valid"};
     }
+    if(password !== passwordConfirm){
+        return {status: 400, message: "Both password don't match"};
+    }
 };
 
 const verifyLoginInput = (email, password) => {
     if(!(email && password)) {
-        return {status: 400, message: "All input is required"};
+        return {status: 400, message: "All inputs are required"};
     }
     if(!emailValidator.validate(email)){
         return {status: 400, message: "Email is not valid"};
