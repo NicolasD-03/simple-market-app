933da976944bc53d72eff8c365e5457b8950ea27
Dick Nicolas <dick.nicolas57@gmail.com>
2021-08-04 23:14:55 +0200

    Refactor main program

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@
 import { showModal, hideModal } from "./modules/modal.js";
 import { createItemCart, clearCart } from "./modules/cart.js";
 import { getItemsMarket, postItemMarket, showItemMarket } from "./modules/market.js";
+import { userLoginRegister } from "./modules/users.js";
+import { verifyToken, decodeToken } from "./modules/token.js";
 
 // Market modal
 const marketModal = document.querySelector("#marketModal");
@@ -10,61 +12,103 @@ const cartButton = document.querySelector("#cartButton");
 // Admin Modal
 const closeAdminModal = document.querySelector("#adminModalClose");
 const adminButton = document.querySelector("#adminButton");
-
 const adminPost = document.querySelector(".admin-form form");
+
+const menu = document.querySelector(".nav-menu");
 
 const marketItems = await getItemsMarket();
 
-let cartList = [
-    {
-        "title": "Banana",
-        "img": "http://localhost:8080/assets/img/Banana-min.png",
-        "price": 5,
-        "number": 10
-    },
-    {
-        "title": "Apple",
-        "img": "http://localhost:8080/assets/img/Apple-min.png",
-        "price": 2,
-        "number": 50
+const url = document.URL;
+
+let userIsConnected = false;
+let decodedToken = null;
+
+const main = async () => {
+    const token = await verifyToken();
+    // Verify if token is valid
+    if(token){
+        userIsConnected = true;
+        decodedToken = decodeToken(token);
+    }else{
+        userIsConnected = false;
     }
-];
+    if(userIsConnected){
+        switch (url) {
+            case "http://localhost:8080/pages/login.html":
+                window.location = "http://localhost:8080/";
+                break;
+            case "http://localhost:8080/pages/register.html":
+                window.location = "http://localhost:8080/";
+                break;
+            default:
+                break;
+        }
+        if(decodedToken.user_admin){
+            const adminButton = document.createElement("div");
+            adminButton.classList.add("nav-menu_item");
+            adminButton.setAttribute("id", "adminButton");
+            adminButton.innerText = "Admin";
 
-cartButton.addEventListener('click', () =>{
-    showModal(marketModal);
-    createItemCart(cartList, cart);
-});
-
-closeMarketModal.addEventListener('click', () =>{
-    hideModal(marketModal);
-    clearCart();
-});
-
-adminButton.addEventListener('click', () =>{
-    showModal(adminModal);
-});
-
-closeAdminModal.addEventListener('click', () =>{
-    hideModal(adminModal);
-});
-
-adminPost.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const { itemTitle,  itemPrice, itemPriceWheight} = e.target.elements;
-    postItemMarket(itemTitle.value, "test", itemPrice.value, itemPriceWheight.value);
-    hideModal(adminModal);
-    itemTitle.value = ''; 
-    itemPrice.value = ''; 
-    itemPriceWheight.value = ''; 
-});
+            menu.appendChild(adminButton)
 
 
+            adminButton.addEventListener('click', () =>{
+                showModal(adminModal);
+            });
+            
+            closeAdminModal.addEventListener('click', () =>{
+                hideModal(adminModal);
+            });
 
-marketItems.forEach(element => {
-    showItemMarket(element);
-});
+            adminPost.addEventListener('submit', (e) =>{
+                e.preventDefault();
+                const { itemTitle,  itemPrice, itemPriceWheight} = e.target.elements;
+                postItemMarket(itemTitle.value, "test", itemPrice.value, itemPriceWheight.value);
+                hideModal(adminModal);
+                itemTitle.value = ''; 
+                itemPrice.value = ''; 
+                itemPriceWheight.value = ''; 
+            });
 
 
+        }
+        const cartButton = document.createElement("div");
+        cartButton.classList.add("nav-menu_item--image");
+        cartButton.setAttribute("id", "cartButton");
+        cartButton.innerHTML = `<svg><use xlink:href="#cart"></use></svg>`;
+
+        menu.appendChild(cartButton);
+
+        cartButton.addEventListener('click', () =>{
+            showModal(marketModal);
+            createItemCart();
+        });
+
+        closeMarketModal.addEventListener('click', () =>{
+            hideModal(marketModal);
+            clearCart();
+        });
+
+    }else{
+        const registerButton = document.createElement("div");
+        registerButton.classList.add("nav-menu_item");
+        registerButton.innerHTML = `<a href="../pages/register.html">Register</a>`
+
+        const loginButton = document.createElement("div");
+        loginButton.classList.add("nav-menu_item");
+        loginButton.innerHTML = `<a href="../pages/login.html">Login</a>`
+
+        menu.appendChild(registerButton);
+        menu.appendChild(loginButton);
+    }
+
+    marketItems.forEach(element => {
+        showItemMarket(element);
+    });
+};
+
+main();
+userLoginRegister();
 
 
 
