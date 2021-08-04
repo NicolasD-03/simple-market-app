78d3d1e10ce866e41a92256c42ef02ae2ff35552
Dick Nicolas <dick.nicolas57@gmail.com>
2021-08-04 23:25:39 +0200

    feat: market api system*
    Refactor partially the market system
    Adding more info in response
    Adding token verification for each request and user status (admin or
    not)

--- src/api/controller/marketController.js
+++ src/api/controller/marketController.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 const { Market } = require('../../models/marketModels.js');
+const { verifyUserAdmin } = require('../../services/verifyUserStatus.js');
+const idVerification = require('../../services/verifyLengthId.js');
 
 module.exports = {
-    createItem: async (name, img, price, wheightPrice) => {
+    createItem: async (req, res) => {
+
+        const { name, img, price, wheightPrice } = req.body;
+        const token = req.get('token');
+
+        if(!await verifyUserAdmin(token)){
+            return res.status(403).json({type: "error", message: "Sorry you can't do this"});
+        }
+
         const item = new Market({
             _id: mongoose.Types.ObjectId(),
             name: name,
@@ -10,12 +20,8 @@ module.exports = {
             price: price,
             wheightPrice: wheightPrice
         });
-        try{
-            const newItemEntry = await item.save();
-            return newItemEntry;
-        }catch (error){
-            return error;
-        }
+        const newItemEntry = await item.save();
+        return res.status(201).json(newItemEntry);
         
     },
     getAllItems: async () => {
@@ -25,19 +31,31 @@ module.exports = {
         });
         return result;
     },
-    getItem: async (id) => {
+    getItem: async (req, res) => {
+        const { id } = req.params; 
+        idVerified = idVerification(id);
+        if(idVerified){
+            return res.status(409).json(idVerified.message)
+        }
         const result = await Market.findById(id, (err, item) => { 
             if(err){ return err };
             return item; 
         });
-        return result;
+        return res.status(200).json(result);
     },
-    deleteItem: async (id) => {
+    deleteItem: async (req, res) => {
+        const { id } = req.params;
+        const token = req.get('token');
+
+        if(!await verifyUserAdmin(token)){
+            return res.status(403).json({type: "error", message: "Sorry you can't do this"});
+        }
+
         const result = await Market.findByIdAndDelete(id, (err, item) => {
             if(err){ return err};
             return item;
         });
-        return result;
+        return res.status(200).json(result);
     },
     updateItem: async (id, body) => {
         const result = await Market.findByIdAndUpdate(id, body, (err, item) => {
--- src/api/routes/market.js
+++ src/api/routes/market.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const marketController = require('../controller/marketController');
-const idVerification = require('../../services/verifyLengthId.js');
-const verifyApiKey = require ('../../services/verifyApiKey.js');
 const router = express.Router();
 
 router.get('/items', async (req, res) => { 
@@ -11,64 +9,32 @@ router.get('/items', async (req, res) => {
 });
 
 router.get('/items/:id', async (req, res) => {
-    const { id } = req.params; 
-    if(idVerification(id)){
-        res.json(idVerification(id));
-        res.status(409);
-    }else{
-        const result = await marketController.getItem(id);
-        res.json(result);
-        res.status(200);
-    }
+    await marketController.getItem(req, res);
 });
 
 router.post('/items', async (req, res) => {
-    const verificationApiKey = verifyApiKey(req);
-    if(verificationApiKey){
-        res.status(verificationApiKey.status).send(verificationApiKey.message);
-    }else{
-        const { name, img, price, wheightPrice } = req.body;
-        const newEntry = await marketController.createItem(name, img, price, wheightPrice);
-        if(newEntry.errors){
-            res.status(400).json(newEntry);
-        }else{
-            res.status(201).json(newEntry);
-        };
-    };
+    await marketController.createItem(req, res);
 });
 
 router.delete('/items/:id', async (req, res) => {
-    const verificationApiKey = verifyApiKey(req);
-    if(verificationApiKey){
-        res.status(verificationApiKey.status).send(verificationApiKey.message);
-    }else{
-        const { id } = req.params;
-        if(idVerification(id)){
-        res.json(idVerification(id));
-        res.status(409);
-        }else{
-        const result = await marketController.deleteItem(id);
-        res.json(result);
-        res.status(200);
-        };
-    };
+    await marketController.deleteItem(req, res);
 });
 
-router.patch('/items/:id', async (req, res) => {
-    const verificationApiKey = verifyApiKey(req);
-    if(verificationApiKey){
-        res.status(verificationApiKey.status).send(verificationApiKey.message);
-    }else{
-        const { id } = req.params;
-        const { body } = req
-        if(idVerification(id)){
-            res.status(409).json(idVerification(id));
-        }else{
-            const result = await marketController.updateItem(id, body);
-            res.status(200).json(result);
-        };
-    };
-});
+// router.patch('/items/:id', async (req, res) => {
+//     const verificationApiKey = verifyApiKey(req);
+//     if(verificationApiKey){
+//         res.status(verificationApiKey.status).send(verificationApiKey.message);
+//     }else{
+//         const { id } = req.params;
+//         const { body } = req
+//         if(idVerification(id)){
+//             res.status(409).json(idVerification(id));
+//         }else{
+//             const result = await marketController.updateItem(id, body);
+//             res.status(200).json(result);
+//         };
+//     };
+// });
 
 
 module.exports = router;
